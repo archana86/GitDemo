66e0b55709eed1c0fde4cfac2da1beb92ccb02bd
Sushma <Sushma@ABG-PC05>
2019-06-21 10:43:36 -0500

    this is my first commit

---
+++ src/GitDemo.java
@@ -0,0 +1,18 @@
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		
+		String hungry= "yes";
+		if(hungry=="yes")
+
+		{
+			System.out.println("eat something");
+		}
+		else
+		{
+			System.out.println("do work");
+		}
+	}
+	}
+
